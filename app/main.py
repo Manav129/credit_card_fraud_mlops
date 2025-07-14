2d44084575a2cf370aea9a11329c358896a9b8fe
nikhilgupta2004 <nikhil.gupta4028@gmail.com>
2025-07-14 19:29:43 +0530

    final working prediction pipeline

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-# app/main.py
-
 from fastapi import FastAPI
 from app.inference import get_prediction, Transaction
 
-app = FastAPI(title="Credit Card Fraud Detection API")
+app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Credit Card Fraud Detection API"}
+    return {"message": "API is running."}
 
 @app.post("/predict")
-def predict(data: Transaction):
-    return get_prediction(data)
+def predict(transaction: Transaction):
+    return get_prediction(transaction)
