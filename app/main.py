22d8277f36f914b22db8ac3dcc492c6a6ec8f5b3
Manav129 <manavgoel888@gmail.com>
2025-08-19 19:59:50 +0530

    Changes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.inference import get_prediction, Transaction
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://credit-card-fraud-frontend-seven.vercel.app"
+    ],  # ✅ Your Vercel frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {"message": "API is running."}
